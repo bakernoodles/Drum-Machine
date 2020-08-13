5a048a29877c134f46cbe287841da3170316e43f
Chris Noods <bakernoodles@hotmail.com>
2020-08-13 13:13:25 -0700

    first upload
    Just starting out the project and uploading the first React create app.

---
+++ src/components/DrumMachine.js
@@ -0,0 +1,14 @@
+import Reacte from 'react';
+
+
+
+const DrumMachine = () => {
+
+    return (
+        <div>
+            <h2>hell again</h2>
+        </div>
+    )
+}
+
+export default DrumMachine;
