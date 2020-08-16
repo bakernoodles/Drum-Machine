949cdecbd76be4b4b1f39ccbdc42cef6432be0b4
Chris Noods <bakernoodles@hotmail.com>
2020-08-15 18:39:19 -0700

    update 8/15
    -cleaned up the original code.
    -organized files.
    -added leftPane component
    -added some css.
    
    project is  now prepped to really start development

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import LeftPane from './components/LeftPane.js';
+import './Styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h2>hello!</h2>
+      <LeftPane/>
     </div>
   );
 }
---
+++ src/components/LeftPane.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftPane = () => {
+   return(
+      <div className="leftPane">
+        test
+      </div>
+   );
+}
+
+export default LeftPane;
